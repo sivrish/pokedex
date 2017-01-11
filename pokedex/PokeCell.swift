47db03527a0d0947ba76af7889d271b72792a1b0
sivrish <sivrishthangamani@gmail.com>
2017-01-11 13:13:53 +0530

    UIPartial

--- pokedex/PokeCell.swift
+++ pokedex/PokeCell.swift
@@ -12,7 +12,11 @@ class PokeCell: UICollectionViewCell {
     @IBOutlet weak var thumbImg: UIImageView!
     @IBOutlet weak var nameLbl: UILabel!
     var pokemon: Pokemon!
-    func configureCell(pokemon: Pokemon){
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        layer.cornerRadius = 5.0
+    }
+    func configureCell(_ pokemon: Pokemon){
         self.pokemon = pokemon
         nameLbl.text = self.pokemon.name.capitalized
         thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
---
+++ pokedex/PokemonDetailVC.swift
@@ -0,0 +1,32 @@
+//
+//  PokemonDetailVC.swift
+//  pokedex
+//
+//  Created by Sivrish Thangamani on 11/01/17.
+//  Copyright © 2017 Sivrish Thangamani. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+    var pokemon: Pokemon!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
